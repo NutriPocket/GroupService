e2af1aef6d975884aaf185fed13695ec6f6809e2
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-05-17 00:43:51 -0300

    build: test environment

---
+++ src/sql/tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS groups (
+    id VARCHAR(36) PRIMARY KEY,
+    owner_id VARCHAR(36) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(512),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+    group_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (group_id, user_id)
+);
