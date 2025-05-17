72759bd8f8c4c11ff143af80e00bd99cfb5bfd4e
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-05-17 00:53:13 -0300

    feat: run tests in ci

---
+++ src/sql/test-ci.sql
@@ -0,0 +1,14 @@
+-- Connect to the default 'postgres' database
+\c postgres;
+
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'test') THEN
+      CREATE DATABASE test;
+   END IF;
+END
+$do$;
+
+-- Connect to the newly created database
+\c test
